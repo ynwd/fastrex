68d585fdff02b3a5fba158fa8915ab78780258fd
ynwdd <10122431+ynwdd@users.noreply.github.com>
2021-06-13 10:02:10 +0700

    update json

--- response.go
+++ response.go
@@ -3,6 +3,7 @@ package fastrex
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -112,6 +113,8 @@ func (h *httpResponse) Json(data interface{}) {
 	var jsonStr string
 
 	switch data.(type) {
+	case string, bool, int, int8, int16, int32, int64, uint8, uint16, uint32, uint64, float32, float64, complex64, complex128:
+		jsonStr = fmt.Sprintf("%v", data)
 	default:
 		jsonStr = processStruct(data)
 	}
